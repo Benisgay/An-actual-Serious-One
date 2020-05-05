a5bffd02ce032022b3695346a42bb2bddabcab80
Benisgay <60450482+Benisgay@users.noreply.github.com>
2020-05-05 13:30:23 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,9 @@
-
+      $(window).on("scroll", function(){
+        if ($(window).scrollTop()){
+          $('nav').addClass("black");
+        }
+        else
+        {
+          $('nav').removeClass("black")
+        }
+     })
